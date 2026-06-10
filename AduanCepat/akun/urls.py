e20cd8bf14662ab0f9c01cee2ef1fabe60d918c1
ihsann <rohmiihsan67@gmail.com>
2026-06-11 02:06:21 +0800

    uas web lanjut kelompook 3

---
+++ AduanCepat/akun/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('forgot-password/', views.forgot_password_view, name='forgot_password'),
+    path('reset-password/', views.reset_password_view, name='reset_password'),
+    path('resend-otp/', views.resend_otp_view, name='resend_otp'),
+    path('reset-success/', views.reset_success_view, name='reset_success'),
+]
