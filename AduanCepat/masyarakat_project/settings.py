fe5705bf9fe972bd6990756b572515dbcf9a96c5
ihsann <rohmiihsan67@gmail.com>
2026-06-30 22:56:49 +0800

    Update Gmail App Password to restore email OTP reset verification

--- AduanCepat/masyarakat_project/settings.py
+++ AduanCepat/masyarakat_project/settings.py
@@ -208,7 +208,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'rohmiihsan6@gmail.com'
-EMAIL_HOST_PASSWORD = 'tzftdhoeqtouluui'
+EMAIL_HOST_PASSWORD = 'aexgnzlxbcitviyv'
 
 # Logging configuration to print OAuth debug info to console
 LOGGING = {
